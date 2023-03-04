013209b662470040ff078554475c060dabd76b19
jakebaz <jake.bazin@googlemail.com>
2023-03-04 10:14:24 +0000

    margin fixes and center content in desktop

--- src/views/WelomeSection.vue
+++ src/views/WelomeSection.vue
@@ -3,49 +3,65 @@
     <div class="main-img">
       <img src="../assets/main-img.jpg" alt="A picture of us" />
     </div>
-    <h1 class="title">
-      <span>Ellie</span>
-      <div class="break">
-        <div class="line line__left"></div>
-        <span class="and">&</span>
-        <div class="line line__right"></div>
+    <div class="content">
+      <div class="title-container">
+        <h1 class="title">
+          <span>Ellie</span>
+          <div class="break">
+            <div class="line line__left"></div>
+            <span class="and">&</span>
+            <div class="line line__right"></div>
+          </div>
+          <span>Jake</span>
+        </h1>
       </div>
-      <span>Jake</span>
-    </h1>
+    </div>
   </div>
 </template>
 <style lang="scss">
 @import "../scss/mixins";
 @import "../scss/variables";
 .welcome-section {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
   .main-img {
     img {
       width: 100%;
     }
   }
-  .title {
-    @include uppercase;
-    @include container;
-    display: flex;
-    flex-direction: column;
-    text-align: center;
-    .break {
-      position: relative;
-      margin: 25px auto;
+  .content {
+    margin: 12px;
+    .title-container {
+      @include uppercase;
+      @include container;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      text-align: center;
       width: 100%;
-      .line {
-        content: "";
-        display: block;
-        width: 40%;
-        height: 3px;
-        position: absolute;
-        top: 50%;
-        background: $text-primary;
-        &__left {
-          left: 0;
-        }
-        &__right {
-          right: 0;
+      .title {
+        width: 100%;
+        margin: auto;
+      }
+      .break {
+        position: relative;
+        margin: 25px auto;
+        width: 100%;
+        .line {
+          content: "";
+          display: block;
+          width: 40%;
+          height: 3px;
+          position: absolute;
+          top: 50%;
+          background: $text-primary;
+          &__left {
+            left: 0;
+          }
+          &__right {
+            right: 0;
+          }
         }
       }
     }
